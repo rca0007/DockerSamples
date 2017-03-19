4dc5d052ad35106ff112191e8ddc733d91e5bcce
Robert Anderson <rca0007@auburn.edu>
2017-03-18 21:50:43 -0500

    Adding infinite cpp program

---
+++ SampleCPPForDocker/InfiniteAdding.cpp
@@ -0,0 +1,14 @@
+
+int main(int len, char** args)
+{
+	while(true)
+	{
+		int oneHundred = 100;
+		int twoHundred = 200;
+		int sum = oneHundred + twoHundred;
+
+		char whatever[] = "whatever!!!!!!!!!!!!!!!";
+	}
+
+	return 0;
+}
